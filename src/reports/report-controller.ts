3ba2e261fa882354578b629702ee5bca1c042f84
juanitojl1988 <juan.japal31@gmail.com>
2024-09-16 20:50:59 -0500

    dfgdgdfg

--- src/reports/report-controller.ts
+++ src/reports/report-controller.ts
@@ -6,6 +6,7 @@ import { FileManager } from 'src/util/util-files';
 import * as fs from 'fs';
 import { JwtService, TokenExpiredError } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
+import path from 'path';
 
 @Controller('')
 export class ReportsController {
@@ -16,7 +17,7 @@ export class ReportsController {
 
 
     @Post('generateReport')
-   // @UseGuards(AuthGuard('api-key'))
+    // @UseGuards(AuthGuard('api-key'))
     async generateReport(@Body() createReportDto: CreateReportDto, @Res() res: Response) {
         try {
 
@@ -34,6 +35,62 @@ export class ReportsController {
         } catch (error) {
             this.logger.error("Error al generateReport, Error: " + error);
             throw error;
+        }
+    }
+
+
+    @Get('view')
+    async viewReport(@Query('token') token: string, @Res() res: Response) {
+
+        try {
+            const decoded = this.jwtService.verify(token);
+
+            const filePath = decoded.pathFile;
+            this.logger.log('Ruta del Archivo: ' + filePath);
+
+            // Verificar si el archivo existe
+            if (!fs.existsSync(filePath)) {
+                this.logger.error('Archivo no encontrado, file: ' + filePath);
+                throw new NotFoundException('Archivo no encontrado, file: ' + filePath);
+            }
+
+            // Obtener la extensión del archivo
+            const fileExtension = path.extname(filePath).toLowerCase();
+
+            // Definir el Content-Type y Content-Disposition basado en la extensión
+            let contentType = 'application/octet-stream'; // Tipo por defecto para archivos binarios
+            let contentDisposition = 'inline'; // Descarga por defecto
+
+            switch (fileExtension) {
+                case '.pdf':
+                    contentType = 'application/pdf';
+                    break;
+                case '.xlsx':
+                    contentType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+                    break;
+                case '.docx':
+                    contentType = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document';
+                    break;
+            }
+
+            // Establecer las cabeceras
+            res.setHeader('Content-Type', contentType);
+            res.setHeader('Content-Disposition', `${contentDisposition}; filename="${path.basename(filePath)}"`);
+
+            // Enviar el archivo
+            res.sendFile(filePath, (err) => {
+                if (err) {
+                    this.logger.error('Error al mostrar/descargar el archivo, Error: ' + err);
+                    throw new InternalServerErrorException(err.message);
+                }
+            });
+
+        } catch (error) {
+            if (error instanceof TokenExpiredError) {
+                throw new InternalServerErrorException('El token ha expirado. Por favor, solicita uno nuevo.');
+            } else {
+                throw new InternalServerErrorException(error.message);
+            }
         }
     }
 
